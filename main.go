6cd152cf879bcbdfeb2195b2a0862367fd34baed
haohanyuzmx <2805890662@qq.com>
2022-07-20 17:51:02 +0800

    add ok1
    add ok2
    
    add oke
    
    add okww
    
    add okww
    
    add okww
    
    test curl
    
    add err
    
    new change
    
    del
    
    del

--- main.go
+++ main.go
@@ -1,17 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"regexp"
+	"net"
 )
 
-// +kubebuilder:validation:Pattern="^(?:(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\/([1-9]|[1-2]\\d|3[0-2])$"
-
 func main() {
-	compile, err := regexp.Compile("^(?:(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\/([1-9]|[1-2]\\d|3[0-2])$")
+	_, err := net.Dial("tcp", "baidu.com")
 	if err != nil {
-		fmt.Println(err)
 		return
 	}
-	fmt.Println(compile.MatchString("10.23.4.56/24"))
 }
