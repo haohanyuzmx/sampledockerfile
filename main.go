e6a14b86d85bddfc4d5cb2bd86e87449da0e7f7b
haohanyuzmx <2805890662@qq.com>
2022-07-20 17:51:02 +0800

    add ok1
    add ok2
    
    add oke
    
    add okww
    
    add okww
    
    add okww
    
    test curl
    
    add err

--- main.go
+++ main.go
@@ -2,16 +2,20 @@ package main
 
 import (
 	"fmt"
-	"regexp"
+	"io"
+	"net/http"
 )
 
-// +kubebuilder:validation:Pattern="^(?:(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\/([1-9]|[1-2]\\d|3[0-2])$"
-
 func main() {
-	compile, err := regexp.Compile("^(?:(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\/([1-9]|[1-2]\\d|3[0-2])$")
+	response, err := http.Get("http://baidu.com")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	fmt.Println(compile.MatchString("10.23.4.56/24"))
+	bytes, err := io.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(bytes))
 }
